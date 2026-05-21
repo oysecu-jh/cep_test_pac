0b41a0bb8d08becb2bc53140a3328368f7fb57cc
oysecu-jh <jh.park214@oliveyoung.co.kr>
2026-05-21 10:08:58 +0900

    Update pac_config.js
    add CBE BO

--- pac_config.js
+++ pac_config.js
@@ -1,11 +1,18 @@
 function FindProxyForURL(url, host) {
   const PROXY = "HTTPS via.prod.securegateway.goog:443";
-  const sites = ["oliveyoungglobal.zendesk.com"]; 
+  
+  // 구글 보안 게이트웨이(Secure Gateway)로 라우팅할 대상 도메인 리스트
+  const sites = [
+    "oliveyoungglobal.zendesk.com",
+    "bo.oliveyoung.com" // 
+  ];
   
   for (const site of sites) {
-    if (shExpMatch(url, 'https://' + site + '/*') || shExpMatch(url, '*.' + site + '/*')) {
+    // 호스트 이름이 정확히 일치하거나(localHostOrDomainIs), 해당 도메인의 하위 서브도메인인 경우(dnsDomainIs)
+    if (localHostOrDomainIs(host, site) || dnsDomainIs(host, site)) {
       return PROXY;
     }
   }
+  
   return 'DIRECT';
 }
