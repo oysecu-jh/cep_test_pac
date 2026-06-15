0cf680be8b2464636b9a5fb76254dda28db8b893
oysecu-jh <jh.park214@oliveyoung.co.kr>
2026-06-15 20:28:19 +0900

    Update pac_config.js

--- pac_config.js
+++ pac_config.js
@@ -1,16 +1,14 @@
 function FindProxyForURL(url, host) {
   const PROXY = "HTTPS via.prod.securegateway.goog:443";
   
-  // 구글 보안 게이트웨이(Secure Gateway)로 라우팅할 대상 도메인 리스트
   const sites = [
     "oliveyoungglobal.zendesk.com",
     "bo.oliveyoung.com",
     "use-bo.oliveyoung.com",
-    "private.oliveyoung.com"// 
+    "private.oliveyoung.com"
   ];
   
   for (const site of sites) {
-    // 호스트 이름이 정확히 일치하거나(localHostOrDomainIs), 해당 도메인의 하위 서브도메인인 경우(dnsDomainIs)
     if (localHostOrDomainIs(host, site) || dnsDomainIs(host, site)) {
       return PROXY;
     }
